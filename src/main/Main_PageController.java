c4f5f6b8c9c1858fc28cda65faa5a396a781dfbf
Queen996 <qurratuaini@Qurratus-MacBook-Air.local>
2018-11-22 11:57:04 +0800

    Hello

---
+++ src/main/Main_PageController.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import java.net.URL;
+import java.util.Random;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+
+public class Main_PageController implements Initializable {
+    
+    @FXML
+    private AnchorPane tree, bg;
+    
+    private Main_PageModel mpm;
+    
+    private Random random = new Random();
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        mpm = new Main_PageModel();
+        mpm.createCircle(bg);
+    }  
+    
+
+    
+}
